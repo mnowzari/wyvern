789fbfb491ecdcf655d88fdf582b5b2c99c5a036
guitarhead429 <guitarhead429@gmail.com>
2024-07-25 16:38:35 -0400

    Refactored how args are parsed by using clap vs. manual arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![allow(dead_code, unused_variables)]
-use std::env;
+use clap::Parser;
 
 mod rw_image;
 mod image_resize;
 mod kmeans;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 struct InputArguments {
+    #[arg(short, long)]
     command: String,
+    #[arg(short, long)]
     path: String,
 }
 
@@ -29,31 +33,14 @@ impl ImageCommand {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let cmd_and_path: InputArguments = parse_args(args);
+    let args: InputArguments = InputArguments::parse();
 
-    let command_to_run: ImageCommand = match cmd_and_path.command {
+    let command_to_run: ImageCommand = match args.command {
         x if x=="kmeans" => ImageCommand::Kmeans,
         x if x=="image_resize" => ImageCommand::ImageResize,
         x if x=="edge_detect" => ImageCommand::EdgeDetect,
         _ => ImageCommand::Quit,
     };
     // run the command the user wants to execute
-    command_to_run.route_command(cmd_and_path.path);
-}
-
-fn parse_args(arguments: Vec<String>) -> InputArguments {
-    let mut args_struct: InputArguments = InputArguments {
-        command: "".to_string(),
-        path: "".to_string(),
-    };
-
-    if arguments.len() == 3 {
-        args_struct.command = arguments[1].to_string();
-        args_struct.path = arguments[2].to_string();
-        args_struct
-    }
-    else {
-        args_struct
-    }
+    command_to_run.route_command(args.path);
 }
