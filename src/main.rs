d440de1c952cf129ef28419e5d61854654d1c5e6
guitarhead429 <guitarhead429@gmail.com>
2024-07-29 21:41:24 -0400

    simplified main.rs arg parsing, still not perfect but getting there

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,82 @@
 #![allow(dead_code, unused_variables)]
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 mod rw_image;
 mod image_resize;
 mod kmeans;
 mod edge_detect;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
 struct InputArguments {
-    command: Option<String>,
+    #[command(subcommand)]
+    command: ImageCommand,
+
     path: Option<String>,
+
     #[arg(short, long)]
-    #[arg(help="threshold for edge detection only")]
-    threshold: Option<f32>,
+    #[arg(default_value_t = 30.0)]
+    #[arg(help="threshold for edge detection.")]
+    threshold: f32,
+
     #[arg(short, long)]
     #[arg(default_value_t = false)]
-    #[arg(help="blackout non-edge pixels in edge detect")]
+    #[arg(help="blackout non-edge pixels in edge detection. Default is False.")]
     blackout: bool,
 }
 
+// #[derive(Parser)]
+// #[command(version, about, long_about = None)]
+// #[command(propagate_version = true)]
+// struct EdgeDetectOptionals {
+//     #[arg(short, long)]
+//     #[arg(default_value_t = 30.0)]
+//     #[arg(help="threshold for edge detection.")]
+//     threshold: f32,
+
+//     #[arg(short, long)]
+//     #[arg(default_value_t = false)]
+//     #[arg(help="blackout non-edge pixels in edge detection. Default is False.")]
+//     blackout: bool,
+// }
+
+#[derive(Subcommand)]
 enum ImageCommand {
     Kmeans,
     ImageResize,
     EdgeDetect,
+    BatchResize,
     Quit,
 }
 
-impl ImageCommand {
-    fn route_command(self, arguments: InputArguments) -> String{
-        match self {
-            ImageCommand::EdgeDetect => {
-                edge_detect::edge_detect(arguments
-                    .path
-                    .expect("No path!"), 
-                arguments.threshold.expect("No threshold!"), 
-                arguments.blackout)
-            },
-            ImageCommand::ImageResize => {
-                image_resize::image_resize(arguments
-                    .path
-                    .expect("No path!"))
-            },
-            ImageCommand::Kmeans => {
-                kmeans::k_means_fast(arguments
-                    .path
-                    .expect("No path!"))
-            },
-            ImageCommand::Quit => {"QUITTING".to_string()},
+fn route_command(arguments: InputArguments) -> String{
+    match arguments.command {
+        ImageCommand::EdgeDetect => {
+            edge_detect::edge_detect(arguments
+                .path
+                .expect("No path!"), 
+            arguments.threshold,
+            arguments.blackout)
+        },
+        ImageCommand::ImageResize => {
+            image_resize::image_resize(arguments
+                .path
+                .expect("No path!"))
+        },
+        ImageCommand::Kmeans => {
+            kmeans::k_means_fast(arguments
+                .path
+                .expect("No path!"))
+        },
+        ImageCommand::BatchResize => {
+            "BATCH RESIZING".to_string()
         }
+        ImageCommand::Quit => {"QUITTING".to_string()},
     }
 }
 
 fn main() {
     let args: InputArguments = InputArguments::parse();
-
-    let command_to_run: ImageCommand = match &args.command {
-        None => ImageCommand::Quit,
-        Some(x) => {
-            if x == "kmeans" {ImageCommand::Kmeans}
-            else if x == "image_resize" {ImageCommand::ImageResize}
-            else if x == "edge_detect" {ImageCommand::EdgeDetect}
-            else {ImageCommand::Quit}
-        }
-    };
-    // run the command the user wants to execute
-    command_to_run.route_command(args);
+    route_command(args);
 }
