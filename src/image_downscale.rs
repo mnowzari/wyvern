90117a4e14025a1c6eee5757a70c0ee6b67c278a
mnowzari <guitarhead429@gmail.com>
2025-01-25 18:11:26 -0500

    More cleanup as per clippy recommendations

--- src/image_downscale.rs
+++ src/image_downscale.rs
@@ -19,22 +19,22 @@ pub fn image_downscale(image_details: &mut ImageDetails) -> Result<bool, Box<dyn
     let mut output_buf: ImageBuffer<Rgb<u8>, Vec<u8>> =
         ImageBuffer::new(width / DOWNSCALE_FACTOR, height / DOWNSCALE_FACTOR);
 
-    let mut i: u32 = 1;
-    while i < width - 1 {
-        let mut k: u32 = 1;
-        while k < height - 1 {
-            let px_top_right: &Rgb<u8> = image_buf.get_pixel(i - 1, k);
-            let px_top_left: &Rgb<u8> = image_buf.get_pixel(i - 1, k - 1);
-            let px_bottom_right: &Rgb<u8> = image_buf.get_pixel(i, k);
-            let px_bottom_left: &Rgb<u8> = image_buf.get_pixel(i, k - 1);
+    let mut row: u32 = 1;
+    while row < width - 1 {
+        let mut col: u32 = 1;
+        while col < height - 1 {
+            let px_top_right: &Rgb<u8> = image_buf.get_pixel(row - 1, col);
+            let px_top_left: &Rgb<u8> = image_buf.get_pixel(row - 1, col - 1);
+            let px_bottom_right: &Rgb<u8> = image_buf.get_pixel(row, col);
+            let px_bottom_left: &Rgb<u8> = image_buf.get_pixel(row, col - 1);
 
             // calculate the average RGB values of the 2x2 grid of pixels
             let px_avg: Rgb<u8> =
                 average_pixel_values(px_top_right, px_top_left, px_bottom_right, px_bottom_left);
-            output_buf.put_pixel(i / DOWNSCALE_FACTOR, k / DOWNSCALE_FACTOR, px_avg);
-            k += 2;
+            output_buf.put_pixel(row / DOWNSCALE_FACTOR, col / DOWNSCALE_FACTOR, px_avg);
+            col += 2;
         }
-        i += 2;
+        row += 2;
     }
 
     image_details.save_image(DynamicImage::ImageRgb8(output_buf), "downscaled")
